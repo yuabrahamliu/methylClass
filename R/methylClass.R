7cdae5b75bd0cc49b25f1cc30212265c95951729
Yu Liu <39322184+yuabrahamliu@users.noreply.github.com>
2023-07-30 14:27:32 -0400

    Add files via upload

---
+++ R/methylClass.R
@@ -0,0 +1,34 @@
+
+#'methylClass: an R package to construct DNA methylation-based classification 
+#'  models
+#'
+#'The R package methylClass is special for performing DNA methylation-based 
+#'  classification. Within it, we provide the eSVM (ensemble-based support 
+#'  vector machine) model to achieve much higher accuracy in methylation data 
+#'  classification than the popular random forest (RF) model and overcome the 
+#'  time-consuming problem of the traditional SVM. In addition, some novel 
+#'  feature selection methods, such as SCMER, are included in the package to 
+#'  improve the classification. Furthermore, we also provide functions for 
+#'  multi-omics studies. 
+#'@section methylClass functions:
+#'  balancesampling
+#'  clustergrid
+#'  clustering
+#'  cvdata
+#'  labelclusters
+#'  mainJvisR
+#'  maincalibration
+#'  maincv
+#'  mainfeature
+#'  mainpredict
+#'  maintrain
+#'  makecv
+#'  multipledistribution
+#'  toDMR
+#'  togene
+#'@docType package
+#'@name methylClass
+NULL
+#> NULL
+
+
